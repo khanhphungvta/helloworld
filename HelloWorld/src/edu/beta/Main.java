4fbc2a663c911d388935713a574319e73667b6da
khanphung.vta <khanphung.vta@gmail.com>
2014-09-29 23:01:41 +0700

    Add a line.

--- HelloWorld/src/edu/beta/Main.java
+++ HelloWorld/src/edu/beta/Main.java
@@ -3,6 +3,7 @@ package edu.beta;
 public class Main {
 
 	public static void main(String[] agrs){
-		System.out.print("Hello World");
+		System.out.print("Hello World!");
+		System.out.println("This is a line.");
 	}
 }
