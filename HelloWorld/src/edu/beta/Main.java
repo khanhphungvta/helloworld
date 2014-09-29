f970281208a71dc794a16ef7700fe05b7bbc6e5d
khanhphungvta <khanhphung.vta@gmail.com>
2014-09-29 23:03:32 +0700

    Change main: add 2nd line.
    Test fetch from github.

--- HelloWorld/src/edu/beta/Main.java
+++ HelloWorld/src/edu/beta/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] agrs){
 		System.out.print("Hello World!");
 		System.out.println("This is a line.");
+		System.out.println("This is 2nd line.");
 	}
 }
