68297f423e75d967c7c083d5f039682edefb7623
khanhphungvta <khanhphung.vta@gmail.com>
2014-10-01 22:33:03 +0700

    Added test 7 from remote

--- HelloWorld/src/edu/beta/Main.java
+++ HelloWorld/src/edu/beta/Main.java
@@ -13,6 +13,7 @@ public class Main {
 		System.out.println("Test 4.");
 		System.out.println("Test 5.");
 		System.out.println("Test 6.");
+		System.out.println("Test 7 from remote.");
 		System.out.println("Added from remote");
 
 	}
